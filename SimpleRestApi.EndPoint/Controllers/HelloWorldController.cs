94c6ee2c191ddb49ef6efa946df4acf10a8b7430
Javier MC <mtzcornejo66@gmail.com>
2022-04-06 23:35:08 -0500

    Added Hello world controller and end point

---
+++ SimpleRestApi.EndPoint/Controllers/HelloWorldController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleRestApi.EndPoint.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello, World.";
+        }
+    }
+}
